ecf3e61109b28851de5f87319001534c90846393
Fadi Safar <fadi.safar@pantheralogistics.com>
2026-03-02 23:58:39 -0500

    initi

--- budgee/app/app.vue
+++ budgee/app/app.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
+  <div class="min-h-screen bg-black">
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="relative overflow-hidden rounded px-4 py-2 text-rose-950 bg-rose-300">
+      <!-- white "scratches" -->
+      <div
+        class="pointer-events-none absolute inset-0 opacity-35 [background-image:repeating-linear-gradient(135deg,rgba(255,255,255,0.9)_0px,rgba(255,255,255,0.9)_1px,transparent_1px,transparent_9px)]"
+      />
+      <span class="relative">hello</span>
+    </div>
   </div>
 </template>
